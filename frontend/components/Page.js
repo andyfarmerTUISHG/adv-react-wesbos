43813e7736f23371470bf16f69eab765bf9e0a98
Andy Farmer <andy.farmer@travelopia.com>
2022-12-24 10:21:19 +0000

    Adding Global Stles, Typography and Layout Styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -2,13 +2,64 @@ import React from 'react';
 import PropTypes from "prop-types";
 import Header from './Header';
 import Footer from './Footer';
+import styled, { createGlobalStyle } from 'styled-components';
 
+const GlobalStyles = createGlobalStyle`
+	@font-face {
+		font-family: 'radnika-next';
+		src: url('/public/static/radnikanext-medium-webfont.woff2');
+		format('woff2');
+		font-weight: normal;
+		font-style: normal;
+	}
+	html {
+		--red:  #ff0000;
+		--black: #393939;
+		--grey: #3a3a3a;
+		--gray: var(--grey);
+		--lightGrey: #e1e1e1;
+		--lightGray: var(--lightGrey);
+		--offWhite: #ededed;
+		--maxWidth: 1000px;
+		--bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+		box-sizing: border-box;
+	}
+	*, *:before, *:after {
+		box-sizing: inherit;
+	}
+	body{
+		font-family: 'radnika-next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		padding: 0;
+		margin: 0;
+		font-size: 1.5rem;
+		line-height: 2;
+
+	}
+	a {
+		text-decoration: none;
+		color: var(--black)
+
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	button {
+		font-family: 'radnika-next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+`;
+const InnerStyles = styled.div`
+	max-width: var(--maxWidth);
+	margin: 0 auto;
+	padding: 2rem;
+`;
 export default function Page({ children }) {
   return (
     <div>
+		<GlobalStyles />
 		<Header />
-      <h2> I am the page Component.</h2>
-      {children}
+		<InnerStyles>
+      		{children}
+		</InnerStyles>
 	  <Footer />
     </div>
   );
