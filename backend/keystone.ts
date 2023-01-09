b2a6e5482c36aec6b9b008204803b6c3504a7e9a
Andy Farmer <andy.farmer@travelopia.com>
2023-01-09 19:57:19 +0000

    Setting up Keystone and Typescript

---
+++ backend/keystone.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config'
+import { config, createSchema } from "@keystone-next/keystone/schema";
+
+const databaseURL = process.env.DATABASE_URL || 'local'
+
+const sessionConfig ={
+	maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
+	secret: process.env.COOKIE_SECRET,
+}
+
+export default config(
+	{
+		server: {
+			cors: {
+				origin: [process.env.FRONTEND_URL],
+				credentials: true,
+			}
+		},
+		db:{
+			adapter: 'mongoose',
+			url: databaseURL,
+			//TODO: Add data seeding here
+		},
+		lists: createSchema({
+			// Schema items go in here
+		}),
+		ui: {
+			//TODO: Change this for roles
+			isAccessAllowed: () => true,
+		}
+		//TODO: Add session values here
+
+	}
+);
