cbe0979a5430dee80736e527b01fdc26a96ac634
Andy Farmer <andy.farmer@travelopia.com>
2023-01-14 16:22:38 +0000

    Setting up Apollo Client - ie glue the App to Data Source BoilerPlate Stuff

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Page from '../components/Page'
 import Router from "next/router";
 import NProgress from "nprogress";
+import "../components/styles/nprogress.css";
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData'
 
 // See https://nextjs.org/docs/api-reference/next/router
 // See https://ricostacruz.com/nprogress/
@@ -10,14 +13,30 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 
-import "../components/styles/nprogress.css";
 
-export default function MyApp({Component, pageProps}) {
-  return (
-	<Page>
-		<Component {...pageProps} />
-	</Page>
+function MyApp({Component, pageProps, apollo}) {
+	console.log(apollo)
+	return (
+		<ApolloProvider client={apollo}>
+		{/* A provider is a high level component that lives high in the app
+		   this allows the children components to gain access to the data */}
+		<Page>
+			<Component {...pageProps} />
+		</Page>
+	</ApolloProvider>
   )
 }
 
+// Boiler Platee Tell Next js to go and get data via
+MyApp.getInitialProps = async  function({Component, ctx}) {
+	let pageProps = {}
+	if (Component.getInitialProps){
+		pageProps = await Component.getInitialProps(ctx)
+	}
+	// Get Any page URL paramas
+	pageProps.query = ctx.query
+	return {pageProps}
+}
 
+// By wrapping the export of MyApp with the withData function this ensures the apollo prop is based into the app
+export default withData(MyApp)
