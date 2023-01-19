5a36d059a1cedf7bed9d60d3c97e4a9b992181aa
Andy Farmer <andy.farmer@travelopia.com>
2023-01-19 19:51:17 +0000

    Fixing and Styling the Nav

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
 		display: grid;
 		grid-template-columns: auto 1fr;
 		justify-content: space-between;
-		align-items: center;
+		align-items: stretch;
 	}
 	.sub-bar {
 		display: grid;
@@ -36,16 +36,16 @@ export default function Header() {
   return (
 	<HeaderStyles>
 		<div className="bar">
-				<Logo>
-			<Link href="/">
-					Sick Fits
-			</Link>
-					</Logo>
+			<Logo>
+				<Link href="/">
+						Sick Fits
+				</Link>
+			</Logo>
+			<Nav />
 		</div>
 		<div className="sub-bar">
 			<p>Search</p>
 		</div>
-			<Nav />
 	</HeaderStyles>
   )
 }
