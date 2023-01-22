3ad339f03e60b2413e88a06a47eaf0386afb8dd5
Andy Farmer <andy.farmer@travelopia.com>
2023-01-22 12:33:40 +0000

    Deleting Products

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,14 +1,17 @@
-import React from 'react'
 import ItemStyles from './styles/ItemStyles'
 import Title from './styles/Title'
 import PriceTag from './styles/PriceTag'
 import Link from 'next/link'
 import formatMoney from '../lib/formatMoney'
+import DeleteProduct from './DeleteProduct'
 
-function Product({product}) {
+export default function Product({ product }) {
   return (
 	<ItemStyles>
-		<img src={product?.photo?.image?.publicUrlTransformed} alt={product.name} />
+		<img
+			src={product?.photo?.image?.publicUrlTransformed}
+			alt={product.name}
+		/>
 		<Title>
 			<Link href={`/product/${product.id}`}>
 				{product.name}
@@ -25,9 +28,10 @@ function Product({product}) {
 				}
 			}}
 			>Edit ✏️</Link>
+			<DeleteProduct id={product.id}>
+				🗑️ Delete
+			</DeleteProduct>
 		</div>
 	</ItemStyles>
   )
 }
-
-export default Product
--- frontend/pages/update.js
+++ frontend/pages/update.js
@@ -1,10 +1,10 @@
 import UpdateProduct from "../components/UpdateProduct";
 
-export default function UpdatePage({query})  {
+export default function UpdatePage({ query }) {
 	console.log(query)
 	return (
 		<div>
-			<UpdateProduct id={query.id}/>
+			<UpdateProduct id={query.id} />
 		</div>
 	)
 }
